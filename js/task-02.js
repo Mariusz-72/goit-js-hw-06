44bcab5b4b62b8c6c704f2583563a64d6c051b5c
Mariusz-72 <mmbwdap@gmail.com>
2023-08-24 19:51:17 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,38 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientsList = document.getElementById('ingredients');
+
+ingredients.forEach(ingredient => {
+  const listItem = document.createElement('li');
+  listItem.textContent = ingredient;
+  listItem.classList.add('item');
+
+  ingredientsList.appendChild(listItem);
+});
+
+
+
+
+//Zadanie zrobione podobnie do przykładu w konspekcie :  
+//const ingredients = [
+//  "Potatoes",
+//  "Mushrooms",
+//  "Garlic",
+//  "Tomatos",
+//  "Herbs",
+//  "Condiments",
+//];
+//const ingredientsList = document.getElementById("ingredients");
+
+//const markup = ingredients
+//  .map((ingredient) => `<li class="item">${ingredient}</li>`)
+//  .join("");
+
+// Wyświetlenie utworzonego markupu w konsoli
+//console.log(markup);
+
+// Dodanie całego markupu w jednej operacji
+//ingredientsList.innerHTML = markup;
