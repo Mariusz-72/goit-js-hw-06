921878774786c6eae9cf49c9d6c3477bac8fcb02
Mariusz-72 <mmbwdap@gmail.com>
2023-08-29 15:32:43 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,19 +1,18 @@
-// Pobierz element z DOM
+
 const validationInput = document.querySelector("#validation-input");
 
-// Funkcja sprawdzająca walidację
-function checkValidation() {
-  const expectedLength = parseInt(validationInput.getAttribute("data-length"));
-  const inputValue = validationInput.value.trim();
 
-  if (inputValue.length === expectedLength) {
+function checkValidation() {
+    const expectedLength = parseInt(validationInput.getAttribute("data-length"));
+    const inputValue = validationInput.value.trim();
+
+    if (inputValue.length === expectedLength) {
     validationInput.classList.remove("invalid");
     validationInput.classList.add("valid");
-  } else {
+        } else {
     validationInput.classList.remove("valid");
     validationInput.classList.add("invalid");
-  }
+        }
 }
 
-// Obsługa zdarzenia blur (utrata fokusu)
 validationInput.addEventListener("blur", checkValidation);
