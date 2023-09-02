9ce910b5ec44b7a759d91562219e47c968cde87d
Mariusz-72 <mmbwdap@gmail.com>
2023-09-02 19:50:37 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -3,8 +3,8 @@ const fontSizeControl = document.getElementById("font-size-control");
 const textSpan = document.getElementById("text");
 
 function changeFontSize() {
-    const fontSizeValue = fontSizeControl.value + "px";
+    const fontSizeValue = fontSizeControl.value + "px"; // dodaję tekst "px" aby wartość była z jednostką px
   textSpan.style.fontSize = fontSizeValue;
 }
 
-fontSizeControl.addEventListener("input", changeFontSize);
+fontSizeControl.addEventListener("input", changeFontSize); // nasłuch na inpucie
