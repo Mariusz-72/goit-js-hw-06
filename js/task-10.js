d74c073f7447c2d568546a6d26e9c06d74d43a2e
Mariusz-72 <mmbwdap@gmail.com>
2023-09-02 20:17:08 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -4,26 +4,27 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 const createButton = document.querySelector("[data-create]");
+// pobranie elementu html z atrybutem data-create i przypisanie go do zmiennej, podobnie pozostałe dwa
 const destroyButton = document.querySelector("[data-destroy]");
 const boxesDiv = document.getElementById("boxes");
 
 createButton.addEventListener("click", createBoxes);
 destroyButton.addEventListener("click", destroyBoxes);
-
+// nasłuch na przyciski 
 function createBoxes() {
-  const amount = document.querySelector("input").value;
+  const amount = document.querySelector("input").value; //do amount wpada ilość podana w input
   let size = 30;
-
+//pętla po zadanej w amount ilości:
   for (let i = 0; i < amount; i++) {
     const box = document.createElement("div");
-    box.style.width = size + "px";
+    box.style.width = size + "px"; //jak w poprzednim zad. musi być dodana jednostka px
     box.style.height = size + "px";
     box.style.backgroundColor = getRandomHexColor();
-    boxesDiv.appendChild(box);
-    size += 10;
+    boxesDiv.appendChild(box); //dodaje nowy box do kontenera div
+    size += 10; //każdy kolejny box będzie większy
   }
 }
 
 function destroyBoxes() {
   boxesDiv.innerHTML = "";
-}
+} // usunięcie boxów z kontenera div
